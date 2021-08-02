ee6f0682c39c8900a1ea13bb1fbcd95508948f2a
Caden Kirby <ckirby@icpms.com>
2021-08-02 16:01:35 -0500

    Add project files.

---
+++ WorkoutWheel/WorkoutWheelForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace WorkoutWheel
+{
+    partial class WorkoutWheelForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonSpin = new System.Windows.Forms.Button();
+            this.timerRotation = new System.Windows.Forms.Timer(this.components);
+            this.circlePictureBox1 = new WorkoutWheel.CirclePictureBox();
+            this.textBoxWorkout = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.circlePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSpin
+            // 
+            this.buttonSpin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSpin.Location = new System.Drawing.Point(522, 361);
+            this.buttonSpin.Name = "buttonSpin";
+            this.buttonSpin.Size = new System.Drawing.Size(83, 53);
+            this.buttonSpin.TabIndex = 1;
+            this.buttonSpin.Text = "Spin!";
+            this.buttonSpin.UseVisualStyleBackColor = true;
+            this.buttonSpin.Click += new System.EventHandler(this.buttonSpin_Click);
+            // 
+            // timerRotation
+            // 
+            this.timerRotation.Tick += new System.EventHandler(this.timerRotation_Tick);
+            // 
+            // circlePictureBox1
+            // 
+            this.circlePictureBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.circlePictureBox1.ImageLocation = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Eight-colour-wheel-2D.p" +
+    "ng/769px-Eight-colour-wheel-2D.png";
+            this.circlePictureBox1.Location = new System.Drawing.Point(173, 6);
+            this.circlePictureBox1.Name = "circlePictureBox1";
+            this.circlePictureBox1.Size = new System.Drawing.Size(764, 764);
+            this.circlePictureBox1.TabIndex = 0;
+            this.circlePictureBox1.TabStop = false;
+            // 
+            // textBoxWorkout
+            // 
+            this.textBoxWorkout.Location = new System.Drawing.Point(17, 41);
+            this.textBoxWorkout.Name = "textBoxWorkout";
+            this.textBoxWorkout.Size = new System.Drawing.Size(165, 20);
+            this.textBoxWorkout.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Workout:";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(18, 67);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(83, 53);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.Location = new System.Drawing.Point(106, 67);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(76, 53);
+            this.buttonSave.TabIndex = 1;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(518, 96);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 24);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Visible = false;
+            // 
+            // WorkoutWheelForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 782);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxWorkout);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonSpin);
+            this.Controls.Add(this.circlePictureBox1);
+            this.Name = "WorkoutWheelForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.circlePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CirclePictureBox circlePictureBox1;
+        private System.Windows.Forms.Button buttonSpin;
+        private System.Windows.Forms.Timer timerRotation;
+        private System.Windows.Forms.TextBox textBoxWorkout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ WorkoutWheel/WorkoutWheelForm.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkoutWheel
+{
+    public partial class WorkoutWheelForm : Form
+    {
+        public WorkoutWheelForm()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> _workouts = new List<string>();
+        private string _filePath;
+
+        #region Buttons and Timers
+        private void buttonSpin_Click(object sender, EventArgs e)
+        {
+            if (timerRotation.Enabled)
+            {
+                labelResult.Text = GetRandomWorkout();
+                labelResult.Visible = true;
+                buttonSpin.Text = "Spin!";
+                timerRotation.Enabled = false;
+            }
+            else
+            {
+                if (!LoadWorkouts()) return;
+                buttonSpin.Text = "Stop!";
+                timerRotation.Enabled = true;
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveWorkouts();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            _workouts.Add(textBoxWorkout.Text);
+            textBoxWorkout.Clear();
+        }
+
+        private void timerRotation_Tick(object sender, EventArgs e)
+        {
+            Image imageToRotate = circlePictureBox1.Image;
+            imageToRotate.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            circlePictureBox1.Image = imageToRotate;
+        }
+        #endregion
+
+        #region Methods
+        private bool LoadWorkouts()
+        {
+            if (File.Exists(_filePath))
+            {
+                ReadWorkoutsFile(_filePath);
+                return true;
+            }
+            else
+            {
+                OpenFileDialog loadDialog = new OpenFileDialog();
+                loadDialog.DefaultExt = ".txt";
+                loadDialog.Filter = "Text Documents (.txt)|*.txt";
+                if (loadDialog.ShowDialog() == DialogResult.OK && File.Exists(loadDialog.FileName))
+                {
+                    _filePath = loadDialog.FileName;
+                    ReadWorkoutsFile(loadDialog.FileName);
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        private void ReadWorkoutsFile(string path)
+        {
+            string lineIn;
+            _workouts.Clear();
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (StreamReader inputReader = new StreamReader(fs))
+            {
+                while ((lineIn = inputReader.ReadLine()) != null)
+                {
+                    _workouts.Add(lineIn);
+                }
+            }
+        }
+
+        private void SaveWorkouts()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var workout in _workouts)
+                sb.Append(workout + "\n");
+            string workouts = sb.ToString();
+
+            if (File.Exists(_filePath))
+            {
+                File.AppendAllText(_filePath, workouts);
+            }
+            else
+            {
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.FileName = "workouts";
+                saveDialog.DefaultExt = ".txt";
+                saveDialog.Filter = "Text Documents (.txt)|*.txt";
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(saveDialog.FileName))
+                        File.AppendAllText(saveDialog.FileName, workouts);
+                    else
+                        File.WriteAllText(saveDialog.FileName, workouts);
+                    _filePath = saveDialog.FileName;
+                }
+            }
+        }
+
+        private string GetRandomWorkout()
+        {
+            int numberOfWorkouts = _workouts.Count;
+            Random r = new Random();
+            int randomNumber = r.Next(numberOfWorkouts);
+            string randomWorkout = _workouts[randomNumber];
+            return randomWorkout;
+        }
+        #endregion
+    }
+}
